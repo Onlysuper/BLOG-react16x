1df4548b7f0b1dd805e86d113b8fc0ddaf71c52e
Onlysuper <2483613372@qq.com>
2019-07-31 15:38:31 +0800

    右侧导航

--- src/Routers.js
+++ src/Routers.js
@@ -1,44 +1,59 @@
 import React from 'react';
 import './App.css';
 import { Layout, Menu, Breadcrumb,Icon,Avatar} from 'antd';
-const { Header, Content, Footer} = Layout;
+const { Header, Content, Footer,Sider} = Layout;
 
 
-function Routers() {
-  return (
-    <Layout>
-    <Header style={{ position: 'fixed', width: '100%',display:'flex' }}>
-      <div style={{ width: '30px', color: '#fff' }}>
-        <Icon style={{ fontSize: 16, color: '#fff' }} type="menu-unfold" />
-      </div>
-      <div className="logo" />
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={['2']}
-        style={{ lineHeight: '64px',flex:'1' }}
+  class Routers extends React.Component {
+  state = {
+    collapsed: true,
+  };
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+  render(){
+    return (
+      <Layout>
+      <Sider
+        className='m-sider'
+        collapsedWidth="0"
+        trigger={null}
+        collapsible
+        collapsed={this.state.collapsed}
       >
-        <Menu.Item key="1">首页</Menu.Item>
-        <Menu.Item key="2">文章</Menu.Item>
-        <Menu.Item key="3">关于</Menu.Item>
-      </Menu>
-      <div>
-      <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>U</Avatar>
-      </div>
-    </Header>
-    <Content style={{ padding: '0 50px', marginTop: 64 }}>
-      <Breadcrumb style={{ margin: '16px 0' }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
-      </Breadcrumb>
-      <div style={{ background: '#fff', padding: 24, minHeight: 380 }}></div>
-    </Content>
-    <Footer style={{ textAlign: 'center' }}>
-      Ant Design ©2016 Created by Ant UED
-    </Footer>
-  </Layout>
-  );
+        <div className="logo" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1">
+            <Icon type="user" />
+            <span>nav 1</span>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Icon type="video-camera" />
+            <span>nav 2</span>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Icon type="upload" />
+            <span>nav 3</span>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout>
+        <Header style={{ background: '#fff', padding: 0 }}>
+          <Icon
+            className="trigger"
+            type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+            onClick={this.toggle}
+          />
+        </Header>
+        <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+    );
+  }
 }
 
 export default Routers;

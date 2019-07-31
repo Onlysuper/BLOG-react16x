2cb63fc56bbd399ac61378c49b85100790ff0f61
Onlysuper <2483613372@qq.com>
2019-07-31 16:50:48 +0800

    布局修改

--- src/Routers.js
+++ src/Routers.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Layout, Menu, Breadcrumb,Icon,Avatar} from 'antd';
+import { Layout, Menu, Breadcrumb,Icon,Avatar,Row, Col} from 'antd';
 const { Header, Content, Footer,Sider} = Layout;
 
 
@@ -40,15 +40,37 @@ const { Header, Content, Footer,Sider} = Layout;
         </Menu>
       </Sider>
       <Layout>
-        <Header style={{ background: '#fff', padding: 0 }}>
+        <Header style={{ padding: '0,30px'}}>
           <Icon
             className="trigger"
+            style={{color:'#fff' }}
             type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
             onClick={this.toggle}
           />
+          <div className='m-pc-head' style={{maxWidth:1000,width:'100%',margin: '0 auto'}}>
+              <div className="logo" />
+              <Menu
+                theme="dark"
+                mode="horizontal"
+                defaultSelectedKeys={['2']}
+                style={{ lineHeight: '64px',flex:'1' }}
+              >
+                <Menu.Item key="1">nav 1</Menu.Item>
+                <Menu.Item key="2">nav 2</Menu.Item>
+                <Menu.Item key="3">nav 3</Menu.Item>
+              </Menu>
+              <div>
+              <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf',textAlign:'center' }}>U</Avatar>
+              </div>
+          </div>
         </Header>
-        <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
-          Content
+        <Content style={{  padding: 24, background: '#fff', minHeight: 280,maxWidth:1000,width:'100%',margin: '24px auto', }}>
+          <div>
+            <Row gutter={16}>
+              <Col xs={24} sm={12} md={16} lg={16} xl={17}>Col</Col>
+              <Col xs={24} sm={12} md={4} lg={8} xl={7}>Col</Col>
+            </Row>
+          </div>
         </Content>
       </Layout>
     </Layout>

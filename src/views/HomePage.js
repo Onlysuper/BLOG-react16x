676928a2ced898b6e48f21454d6c58598a7cbb08
Onlysuper <2483613372@qq.com>
2019-08-01 18:11:06 +0800

    添加二次元

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -9,6 +9,8 @@ class HomePage extends React.Component{
     super()
   }
   state = {
+    loadingArticle:false,
+    loadingUser:false,
     loading:false,
     articleList:[],
     userList:[]
@@ -18,41 +20,47 @@ class HomePage extends React.Component{
     this.getUsers();
   }
   getArticles(){
-    this.setState({loading :true})
+    this.setState({loadingArticle :true})
     getAriticle({}).then(data=>{
       this.setState({
         articleList :data
       })
-      this.setState({loading :false})
+      this.setState({loadingArticle :false})
     }).catch(error=>{
-      this.setState({loading :false})
+      this.setState({loadingArticle :false})
     })
   }
   getUsers(){
-    this.setState({loading :true})
+    this.setState({loadingUser :true})
     getUsers({}).then(data=>{
       this.setState({
         userList:data
       })
-      this.setState({loading :false})
+      this.setState({loadingUser :false})
     }).catch(error=>{
-      this.setState({loading :false})
+      this.setState({loadingUser :false})
     })
   }
   render(){
       return (
-        <Spin spinning={this.state.loading}>
+      
           <Row style={{minWidth:300}} gutter={16}>
             <Col xs={24} sm={12} md={16} lg={16} xl={17}>
-              <HomeList articleList={this.state.articleList}/>
-            <Button type="dashed" style={{width:'100%'}}>阅读更多</Button>
+              <Spin spinning={this.state.loadingArticle}>
+                <HomeList articleList={this.state.articleList}/>
+                <Button type="dashed" style={{width:'100%'}}>阅读更多</Button>
+              </Spin>
             </Col>
             <Col xs={24} sm={12} md={4} lg={8} xl={7}>
+              <Spin spinning={this.state.loadingUser}>
               <HomeCard title="作者榜" articleList={this.state.userList}/>
+              </Spin>
+              <Spin spinning={this.state.loadingUser}>
               <HomeCard title="作者榜" articleList={this.state.userList}/>
+              </Spin>
             </Col>
           </Row>
-        </Spin>
+       
         )
   }
 }
